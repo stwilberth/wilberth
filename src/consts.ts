5595c2f184ce7a9a0db5ef48b5e92f9de8af5c21
Wilberth <stwilberth@gmail.com>
2025-01-13 19:18:35 -0600

    seo

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Wilberth.com';
-export const SITE_DESCRIPTION = 'Bienvenido a mi sitio web!';
+export const SITE_SUBTITLE  = 'Programador web y asesor en marketing digital';
+export const SITE_DESCRIPTION = 'Creo páginas web combinando mi experiencia y el poder de la inteligencia artificial, logrando maximizar resultados con soluciones innovadoras, eficientes y personalizadas.';
