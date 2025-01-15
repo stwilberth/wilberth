524d83acfc65073a7fa4d2ad86df15880dd32c7d
Wilberth <stwilberth@gmail.com>
2025-01-14 20:07:03 -0600

    description

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Wilberth.com';
-export const SITE_SUBTITLE  = 'Programador web y asesor en marketing digital';
+export const SITE_SUBTITLE  = 'Creación de sitios web y apps';
 export const SITE_DESCRIPTION = 'Creo páginas web combinando mi experiencia y el poder de la inteligencia artificial, logrando maximizar resultados con soluciones innovadoras, eficientes y personalizadas.';
