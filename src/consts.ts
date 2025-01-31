095e186e824a12164cb12e80f6c390b14af6594b
Wilberth <stwilberth@gmail.com>
2025-01-31 10:22:44 -0600

    changes contact

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,4 @@ export const SITE_TITLE = 'Wilberth.com';
 export const SITE_SUBTITLE = 'Desarrollo web, email marketing y SEO';
 export const SITE_DESCRIPTION = 'Wilberth.com es una empresa de desarrollo web, email marketing y SEO. Ofrecemos servicios de creación de sitios web, app, marketing de email y optimización de SEO. Nuestro objetivo es ayudar a nuestros clientes a crecer online y a alcanzar sus objetivos de negocio.';
 export const SITE_PHONE_NUMBER = '50685008393';
+export const SITE_WHATSAPP_NUMBER = '50685008393';
