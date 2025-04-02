512cdb9075a8d24245ef6fdf1d22974f83965075
Wilberth Loría <stwilberth@gmail.com>
2025-04-02 17:38:43 -0600

    f

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,4 @@ export const SITE_TITLE = 'Wilberth.com';
 export const SITE_SUBTITLE = 'Diseño web, email y SEO';
 export const SITE_PHONE_NUMBER = '50685008393';
 export const SITE_WHATSAPP_NUMBER = '50685008393';
+export const SITE_DESCRIPTION = 'Ofrezco servicios de creación de sitios web, apps, email y SEO. Mi objetivo es ayudar a mis clientes a crecer en internet y a alcanzar sus objetivos de negocio.';
